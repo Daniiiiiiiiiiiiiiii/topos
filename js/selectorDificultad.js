d0e3b4e5211990430a9cbe2d72778d13a5d96339
Dani <ploejem38@gmail.com>
2023-11-17 12:39:17 +0100

    topos

--- js/selectorDificultad.js
+++ js/selectorDificultad.js
@@ -1,21 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dificultad = localStorage.getItem("dificultad");
+  console.log(dificultad);
 
-const dificultad = localStorage.getItem('dificultad');
-console.log(dificultad);
-
-let modo = '';
-switch (dificultad) {
-    case 'facil':
-        modo = 'js/modoFacil.js';
-        break;
-    case 'media':
-        modo = 'js/modoMedio.js';
-        break;
-    case 'dificil':
-        modo = 'js/modoDificil.js';
-        break;
+  let modo = "";
+  switch (dificultad) {
+    case "facil":
+      modo = "js/modoFacil.js";
+      break;
+    case "media":
+      modo = "js/modoMedio.js";
+      break;
+    case "dificil":
+      modo = "js/modoDificil.js";
+      break;
     default:
-        modo = 'js/modoFacil.js';
-}
-const script = document.createElement('script');
-script.src = modo;
-document.body.appendChild(script);
+      modo = "js/modoFacil.js";
+  }
+  const script = document.createElement("script");
+  script.src = modo;
+  document.body.appendChild(script);
+});
