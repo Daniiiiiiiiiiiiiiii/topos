0a1889fcee0c51fef68f260efe7c473ff0c0094b
Dani <ploejem38@gmail.com>
2023-11-22 12:49:37 +0100

    topos

---
+++ js/leaderBoard.js
@@ -0,0 +1,34 @@
+var jugadores = JSON.parse(localStorage.getItem("Jugador"));
+console.log(jugadores);
+
+function agregarJugadoresEnCajas() {
+  if (jugadores) {
+    jugadores.forEach(function (jugador) {
+      var jugadorElemento = document.createElement("div");
+      jugadorElemento.id = 'jugador';
+      jugadorElemento.textContent = jugador.nombre + " ... " + jugador.puntuacion;
+
+      var cajaId = "";
+      switch (jugador.dificultad) {
+        case "facil":
+          cajaId = "facil";
+          break;
+        case "media":
+          cajaId = "media";
+          break;
+        case "dificil":
+          cajaId = "dificil";
+          break;
+        default:
+          console.log("no funciona");
+          break;
+      }
+
+      var caja = document.getElementById(cajaId);
+      caja.appendChild(jugadorElemento);
+    });
+  } else {
+    console.log("no entra jugadores...");
+  }
+}
+agregarJugadoresEnCajas();
